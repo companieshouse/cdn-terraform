516eb8fde815a0505db213d33da57029d06d976b
Marc Ransome <mransome@companieshouse.gov.uk>
2024-03-27 08:07:03 +0000

    Switch to account level resources

--- groups/cdn/s3.tf
+++ groups/cdn/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "${var.environment}.${var.service}.${var.aws_account}.ch.gov.uk"
+  bucket = "${var.service}.${var.aws_account}.ch.gov.uk"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
