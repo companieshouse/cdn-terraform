9b474cf9a9ce3a24757d419a2bd6a98b418e120a
elliseggs11 <jellis@companieshouse.gov.uk>
2024-04-02 13:45:37 +0100

    ssl policy and server access logging enabled

--- groups/cdn/s3.tf
+++ groups/cdn/s3.tf
@@ -35,6 +35,43 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   policy = data.aws_iam_policy_document.s3_cloudfront_policy.json
 }
 
+resource "aws_s3_bucket" "cdn_log_bucket" {
+  bucket = "chs_cdn_server_access_logging"
+}
+resource "aws_s3_bucket_logging" "s3_cdn_logging" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  target_bucket = aws_s3_bucket.cdn_log_bucket.id
+  target_prefix = "log/"
+}
+
+resource "aws_s3_bucket_acl" "log_bucket_acl" {
+  bucket = aws_s3_bucket.cdn_log_bucket.id
+  acl    = "log-delivery-write"
+}
+
+data "aws_iam_policy_document" "s3_ssl_policy" {
+  statement {
+    sid    = "AllowSSLRequestsOnly"
+    effect = "Deny"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+    actions = [
+      "s3:*",
+    ]
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}/*"
+    ]
+  }
+}
+
 data "aws_iam_policy_document" "s3_cloudfront_policy" {
   statement {
 
