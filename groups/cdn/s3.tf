63f0c028a17796912e65cfbf791b4a1cf15d4437
Marc Ransome <mransome@companieshouse.gov.uk>
2024-04-25 07:42:09 +0100

    Reorder bucket related resources

--- groups/cdn/s3.tf
+++ groups/cdn/s3.tf
@@ -2,16 +2,11 @@ resource "aws_s3_bucket" "assets" {
   bucket = "${var.service}.${var.aws_account}.ch.gov.uk"
 }
 
-#trivy:ignore:AVD-AWS-0132
-resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
+resource "aws_s3_bucket_logging" "assets" {
   bucket = aws_s3_bucket.assets.id
 
-  rule {
-    bucket_key_enabled = true
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
+  target_bucket = aws_s3_bucket.logs.id
+  target_prefix = "logs/"
 }
 
 resource "aws_s3_bucket_ownership_controls" "assets" {
@@ -20,6 +15,11 @@ resource "aws_s3_bucket_ownership_controls" "assets" {
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
+}
+
+resource "aws_s3_bucket_policy" "assets" {
+  bucket = aws_s3_bucket.assets.id
+  policy = data.aws_iam_policy_document.assets.json
 }
 
 resource "aws_s3_bucket_public_access_block" "assets" {
@@ -31,13 +31,42 @@ resource "aws_s3_bucket_public_access_block" "assets" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_policy" "assets" {
+#trivy:ignore:AVD-AWS-0132
+resource "aws_s3_bucket_server_side_encryption_configuration" "assets" {
   bucket = aws_s3_bucket.assets.id
-  policy = data.aws_iam_policy_document.assets.json
+
+  rule {
+    bucket_key_enabled = true
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
 
 resource "aws_s3_bucket" "logs" {
   bucket = "${var.service}-access-logs.${var.aws_account}.ch.gov.uk"
+}
+
+resource "aws_s3_bucket_ownership_controls" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_policy" "logs" {
+  bucket = aws_s3_bucket.logs.id
+  policy = data.aws_iam_policy_document.logs.json
+}
+
+resource "aws_s3_bucket_public_access_block" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 #trivy:ignore:AVD-AWS-0132
@@ -50,33 +79,4 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
       sse_algorithm = "AES256"
     }
   }
-}
-
-resource "aws_s3_bucket_public_access_block" "logs" {
-  bucket = aws_s3_bucket.logs.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_ownership_controls" "logs" {
-  bucket = aws_s3_bucket.logs.id
-
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
-}
-
-resource "aws_s3_bucket_logging" "assets" {
-  bucket = aws_s3_bucket.assets.id
-
-  target_bucket = aws_s3_bucket.logs.id
-  target_prefix = "logs/"
-}
-
-resource "aws_s3_bucket_policy" "logs" {
-  bucket = aws_s3_bucket.logs.id
-  policy = data.aws_iam_policy_document.logs.json
 }
