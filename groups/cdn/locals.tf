bb2a8e2a618a9ff93658a81ed2147954dceb8a2a
Marc Ransome <mransome@companieshouse.gov.uk>
2024-05-08 13:58:42 +0100

    Reformat source files

--- groups/cdn/locals.tf
+++ groups/cdn/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  aws_account_id = data.aws_caller_identity.current.account_id
+  aws_account_id              = data.aws_caller_identity.current.account_id
   cloudfront_shared_origin_id = "${var.service}-${var.aws_account}-shared-origin"
 }
